bf6c850a5a0280c64cf95d650386b114567a039b
Chen Dong <raycdut@hotmail.com>
2016-07-09 08:31:13 +0800

    ass

---
+++ slnDemo/src/Demo/Models/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo.Models
+{
+    using System.Collections.Generic;
+
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+
+        IEnumerable<TodoItem> GetAll();
+
+        TodoItem Find(string key);
+
+        TodoItem Remove(string key);
+
+        void Update(TodoItem item);
+    }
+}
---
+++ slnDemo/src/Demo/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Models
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
